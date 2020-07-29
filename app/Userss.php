d1b2675413415f97bce09846a010b516eb93c365
Imam Try Wibowo <imamtrywibowo@gmail.com>
2020-07-29 23:54:57 +0700

    commit

--- app/Userss.php
+++ app/Userss.php
@@ -3,13 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 // api authentication (3)
 use Laravel\Passport\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
     // api authentication (4)
-    use HasApiTokens;
+    use HasApiTokens, Notifiable;
     //
 
     /**
