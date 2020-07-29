8966dcbe09924660ad4a08141459757f2df4c2e5
Imam Try Wibowo <imamtrywibowo@gmail.com>
2020-07-29 23:23:20 +0700

    change model Userss

--- app/Userss.php
+++ app/Userss.php
@@ -11,4 +11,13 @@ class User extends Model
     // api authentication (4)
     use HasApiTokens;
     //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 }
